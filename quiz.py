25e85d5d2d4221e30366189a16976b1e2376bf5f
AlistairDev <gen-ass@users.noreply.github.com>
2017-08-02 12:17:15 +0200

    Input and Output descriptions added to the def sections

--- quiz.py
+++ quiz.py
@@ -23,6 +23,9 @@ options = []
 #F ------------------- Defining the variables and methods that wil be used in the game -------------
 def game():
     #1 - Introduction section
+
+    # Input - Users are asked to enter difficulty by prompting the options level.
+    # Output - User is given a response of what his option was, if the response is invalid user is prompted to enter valid one.
     user_input = "Note: ANSWERS are CaSe SeNsitive\nPlease select a game difficulty by typing it in!\nPossible options:  level1  level2  level3 \nPlease type your game difficulty:"
     difficulty = raw_input(user_input)
     instructions = "\nYour selection was '" + str(difficulty) + "'."
@@ -36,6 +39,9 @@ def game():
 
 def option(difficulty,option_index,guesses):
     #2 - Here a player inputs his answer: level1, level2, level3
+
+    # Input - When user inputs difficulty in def game() the def option() is called. 
+    # Output - The level of difficulty is then passed back to the user prompt. If he selects an invalid statement then the user is prompted to select a valid option again.
     print game_text[difficulty]
     if option_index == len(answers[difficulty]):
         return win(guesses)
@@ -47,6 +53,9 @@ def option(difficulty,option_index,guesses):
 
 def start(difficulty):
     #3 - The player gets the option of selecting how many chioces he gets before losing.
+
+    # Input - User is prompted to select how many times guesses can be made
+    # Output - The guesses are then displayed to the user of how many times incorrect options can be made before it is game over
     guesses_prompt = "Please select the number of guesses you'd like:"
     guesses = raw_input(guesses_prompt)
     if int(guesses):
@@ -57,11 +66,17 @@ def start(difficulty):
 
 def win(guesses):
     #4 - When a payer wins the game
+
+    # Input - Correct guesses are counted
+    # Output - Guesses are then returned as a prompt on the screen indicating the player has won and with how many guesses remains.
     winning = "You've won and : " + str(guesses) + " option(s) remain(s)\nVisit the victory video(not spam or malicious, just some silliness): https://youtu.be/1npWhzBJAzA\n"
     return winning
 
 def correct(option_index,difficulty,guesses):
     #5 - When the correct answer is made
+
+    # Input - The user inputs an answer if correct this will run
+    # Output - User gets a screen prompt that a correct answer has been made
     correct_answer = "\nCorrect! '" + str(options[option_index]) + "' is the correct answer!"
     print correct_answer
     game_text[difficulty] = game_text[difficulty].replace(containers[option_index],options[option_index])
@@ -69,8 +84,12 @@ def correct(option_index,difficulty,guesses):
 
 def incorrect(guesses,difficulty,option_index):
     #6 - What will happen if the player has made incorrect options
+
+    # Input - The user inputs an answer if incorrect this will run
+    # Output - User gets a screen prompt that an incorrect answer has been made and a prompt tells user how many guess attempts remain. If the attempts are exceeded then a prompt is given that the game is over
     incorrect_option = options.pop()
-    if guesses == 0:
+    min_guesses = 0
+    if guesses == min_guesses:
         game_over = "You've guessed incorrectly too many times\nTry again!"
         return game_over
     else:
@@ -78,7 +97,6 @@ def incorrect(guesses,difficulty,option_index):
         incorrect_answer_text = "\n'" + incorrect_option + "' unfortunately that is incorrect!\nTry again, guesses remaining : " + str(guesses) 
         print incorrect_answer_text
         return option(difficulty,option_index,guesses)
-
 
 #7. Run game program
 print game()
